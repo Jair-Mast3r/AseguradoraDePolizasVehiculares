68cd9e607c910c40b31813a9e248dbc4539b1fa1
Jair-Mast3r <hiram.ramirezsn@yahoo.com>
2024-05-02 08:29:26 -0600

    Wrong validation within Inmueble class deleted

--- src/seguros/Inmueble.java
+++ src/seguros/Inmueble.java
@@ -5,9 +5,6 @@ public class Inmueble extends Polizas {
     
     public Inmueble(String num_poliza, String fecha_inicial, String fecha_final, double costo, int valor) {
     	super(num_poliza, fecha_inicial, fecha_final, costo);
-    	if (costo < 0) {
-        	throw new IllegalArgumentException("No puedes tener un costo negativo");
-        }
         this.valor = valor;
     }
     
